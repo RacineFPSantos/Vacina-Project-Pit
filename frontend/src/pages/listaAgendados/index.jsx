673c8a6ff8d44c3cad99ce07cd9b7e5d318d0778
Racine Santos <felipe.xdsam@gmail.com>
2021-04-17 06:03:05 -0300

    Date filter add

--- frontend/src/pages/listaAgendados/index.jsx
+++ frontend/src/pages/listaAgendados/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { format } from 'date-fns';
 import Page from '../../components/page';
 import axios from '../../utils/api';
 
@@ -6,6 +7,23 @@ import Table from '../../components/listPatient';
 
 const index = () => {
   const [patientsList, setPatientsList] = useState([]);
+  const [currentList, setCurrentList] = useState([]);
+
+  const patientsOfTheDay = (date) => {
+    if (patientsList === null) {
+      console.log('Check before');
+    }
+
+    const otherDate = format(date, 'MM-dd-yy');
+
+    const list = patientsList.filter(
+      (patient) =>
+        new Date(otherDate).getTime() ===
+        new Date(format(new Date(patient.dateVaccine), 'MM-dd-yyy')).getTime(),
+    );
+
+    setCurrentList(list);
+  };
 
   const fetchData = async () => {
     try {
@@ -23,7 +41,18 @@ const index = () => {
   return (
     <Page>
       <div>
-        <Table patientsList={patientsList} />
+        <div className="btn-date">
+          <button type="button" onClick={() => patientsOfTheDay(new Date())}>
+            Ontem
+          </button>
+          <button type="button" onClick={() => patientsOfTheDay(new Date())}>
+            Hoje
+          </button>
+          <button type="button" onClick={() => patientsOfTheDay(new Date())}>
+            Amanha
+          </button>
+        </div>
+        <Table currentList={currentList} trigger={patientsOfTheDay} />
       </div>
     </Page>
   );
